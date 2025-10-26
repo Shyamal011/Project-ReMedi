90e94039a77517c40831a292d7c9a609bb8722bf
Shyamal011 <shyamalsantosh2006@gmail.com>
2025-10-26 21:17:52 +0530

    Edit Reminder page + few more changes

--- app/src/main/java/com/app/project_remedi/Reminders.java
+++ app/src/main/java/com/app/project_remedi/Reminders.java
@@ -3,6 +3,8 @@ package com.app.project_remedi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +13,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Reminders extends AppCompatActivity {
+    private Button addbutton;
+    private Button editbutton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_reminders);
+
+        addbutton = findViewById(R.id.addbutton1);
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Reminders.this, AddMedications.class);
+                startActivity(intent);
+            }
+        });
+
+        editbutton = findViewById(R.id.editbutton);
+        editbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Reminders.this, EditReminder.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
